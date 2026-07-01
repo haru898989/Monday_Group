9d9cf3475627f327f502116837af55b0c7eb449c
Koki Yanagida <e1c24105oit.ac.jp>
2026-07-01 12:16:04 +0900

    feat：ギミックのベースの追加

---
+++ Play_Photo/Assets/Main/Script/Gimmick/HumanGimmick.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanGimmick : MonoBehaviour, GimmickBase
+{
+    public void ActivateMagic()
+    {}
+}
