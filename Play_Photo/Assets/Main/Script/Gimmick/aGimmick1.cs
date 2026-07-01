838f19dff3b2ebd3d7f9f01f69cd343d98bfcd16
Kouta0425 <e1c24055@st.oit.ac.jp>
2026-07-01 14:29:03 +0900

    feat：タッチ機能追加

--- Play_Photo/Assets/Main/Script/Gimmick/aGimmick1.cs
+++ Play_Photo/Assets/Main/Script/Gimmick/aGimmick1.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HumanGimmick : MonoBehaviour, GimmickBase
+public class aGimmick1 : MonoBehaviour, GimmickBase
 {
     public void ActivateMagic()
     {
-        Debug.Log("ギミック発動");
+        Debug.Log("ギミ");
     }
 }
